c89459937e50cf2c9dcd39663a4e42ef87496a6c
Francisco <francisco.vergarat@gmail.com>
2019-07-30 11:14:56 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# fibonacci_recur_cs
-Fibinacci using recursion in C#
+# Fibinacci using Recursion
+
+## Overview:
+Simple fibonacci sequence for  both positive and negative numbers using recursion. 
+
+### Installation Instructions: 
+
+#### .Net Core:
+
+To start using this project you will need to install .Net Core.
+
+[Download .Net Core](https://dotnet.microsoft.com/download "MS .Net Core download page")
+
+If you already had .Net Core installed, make sure you have the latest version installed.
+
+
+Once you have installed dotNet Core, open your terminal and type:
+
+```bash
+git clone https://github.com/MorbidValkyria/fibonacci_recur_cs.git
+
+```
+
+Now change the directory to the project:
+
+```bash
+cd fibonacci_recur_cs/Fibonacci
+```
+
+### How to run:
+
+Run the project from the termnal:
+```csharp
+dotnet run
+```
+
+
+
+
---
+++ fibonacci/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+/// <summary>
+/// -------------------------------------------------------------------------------
+/// Fibonacci Sequences using recursion and Dictionaries
+/// My Algorithm calculates both positive and negative integers
+/// 
+/// Tested with ranges from -20000 to 20000 that worked
+/// 
+/// Ranges after -30000 to 30000 gave a stack-overflow exeption so, 
+/// around (-20000 to 20000) should be the limit!
+/// -------------------------------------------------------------------------------
+/// </summary>
+
+namespace fibonacci
+{
+    class Program
+    {
+        /*
+            Main function (entry point)
+        */
+        static void Main(string[] args)
+        {
+            // Created a Dic to store previous results and easy up the computational process
+            // (Which in recursion is something important) considering the amount of memory it eats.
+            // The key is of type int and the value of type BigInteger which should have no upper
+            // or lower bounds in terms of size. 
+            Dictionary<int, BigInteger> fibDict = new Dictionary<int, BigInteger>();
+            // Stored the fibs of 0 and 1 to serve as a starting point (and so the compiler desn't have to)
+            fibDict.Add(0, 0);
+            fibDict.Add(1, 1);
+
+            Console.WriteLine(NewFib(20000, fibDict));
+        }
+        public static BigInteger NewFib(int n, Dictionary<int, BigInteger> fibDict)
+        {
+            BigInteger ans;
+            // Calculates negative integers
+            if (n < 0)
+            {
+                int sign =  n % 2 == 0 ? -1 : 1;
+                ans = sign * NewFib(-n, fibDict);
+                fibDict.Add(n, ans);
+                return ans;
+            }
+            // If it already has calculated that fib numbers, just returns it
+            // instead of doing the calculations all over again
+            if (fibDict.ContainsKey(n)) return fibDict[n];
+
+            // Calculates positive integers
+            else
+            {
+                ans = NewFib(n-1, fibDict) + NewFib(n-2, fibDict);
+                fibDict.Add(n, ans);
+                return ans;
+            }
+        }
+    }
+}
